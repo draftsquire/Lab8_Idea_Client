bad710a6950760b54f93eb42ea1e067202809c22
draftsquire <56137870+draftsquire@users.noreply.github.com>
2020-07-05 14:24:13 +0300

    Изменена версия jdk на 8 Добавлено графическое окно

--- Lab8 Idea Client/src/com/company/Main.java
+++ Lab8 Idea Client/src/com/company/Main.java
@@ -3,10 +3,15 @@ package com.company;
 import java.io.*;
 import java.net.*;
 import java.sql.SQLOutput;
-//BLAblabla
-public class Main {/// NAZILOST
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ClientSession session = new ClientSession();
+        Application.launch();
         SocketAddress a = new InetSocketAddress(InetAddress.getByName("localhost"), 4445);
         DatagramSocket s = new DatagramSocket();
         byte[] b = Converter.convertToBytes(new Query("get_names"));
@@ -34,5 +39,13 @@ public class Main {/// NAZILOST
             Validator.SetUsers(reply.getUsers());
             System.out.println("\n "+ reply.getStringOutput());
         }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX application");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(400);
+        primaryStage.show();
     }
 }
